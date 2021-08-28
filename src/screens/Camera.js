71cb91ff36e61bee2683f627d9525571335e93a1
Anna Lai <annateresa.lai@mail.utoronto.ca>
2021-08-28 16:39:35 -0400

    Update Camera so no error

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,7 +1,9 @@
 // With reference to: https://www.codegrepper.com/code-examples/javascript/react+native+camera+expo
 
-import React from "react";
-import { View } from "react-native";
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
 import {
   Layout,
   TopNav,
@@ -11,28 +13,37 @@ import {
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black",
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20,
+  },
+});
+
 export default function ({ navigation }) {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black',
-    },
-    preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
-    capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      padding: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'center',
-      margin: 20,
-    },
-  });
+  
+   takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 
   const { isDarkmode, setTheme } = useTheme();
   return (
@@ -73,42 +84,42 @@ export default function ({ navigation }) {
         <Text fontWeight="bold">Take a picture here</Text>
 
         <RNCamera
-                    ref={ref => {
-                      this.camera = ref;
-                    }}
-                    style={styles.preview}
-                    type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
-                    androidCameraPermissionOptions={{
-                      title: 'Permission to use camera',
-                      message: 'We need your permission to use your camera',
-                      buttonPositive: 'Ok',
-                      buttonNegative: 'Cancel',
-                    }}
-                    androidRecordAudioPermissionOptions={{
-                      title: 'Permission to use audio recording',
-                      message: 'We need your permission to use your audio',
-                      buttonPositive: 'Ok',
-                      buttonNegative: 'Cancel',
-                    }}
-                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
-                      console.log(barcodes);
-                    }}
-                  />
-                  <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                    <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-                      <Text style={{ fontSize: 14 }}> SNAP </Text>
-                    </TouchableOpacity>
-                  </View>
-
-
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: "Permission to use camera",
+            message: "We need your permission to use your camera",
+            buttonPositive: "Ok",
+            buttonNegative: "Cancel",
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: "Permission to use audio recording",
+            message: "We need your permission to use your audio",
+            buttonPositive: "Ok",
+            buttonNegative: "Cancel",
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Layout>
   );
 }
-
-
-
 
 // ---------------------------------------
 // 'use strict';
