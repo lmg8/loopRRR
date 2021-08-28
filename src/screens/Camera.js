083324b95e5bde21b4d75ee926e3090a48ab5941
Anna Lai <annateresa.lai@mail.utoronto.ca>
2021-08-28 14:45:59 -0400

    Create Camera.js

---
+++ src/screens/Camera.js
@@ -0,0 +1,186 @@
+// With reference to: https://www.codegrepper.com/code-examples/javascript/react+native+camera+expo
+
+import React from "react";
+import { View } from "react-native";
+import {
+  Layout,
+  TopNav,
+  Text,
+  themeColor,
+  useTheme,
+} from "react-native-rapi-ui";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function ({ navigation }) {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'black',
+    },
+    preview: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    capture: {
+      flex: 0,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      padding: 15,
+      paddingHorizontal: 20,
+      alignSelf: 'center',
+      margin: 20,
+    },
+  });
+
+  const { isDarkmode, setTheme } = useTheme();
+  return (
+    <Layout>
+      <TopNav
+        middleContent="Recycle"
+        leftContent={
+          <Ionicons
+            name="chevron-back"
+            size={20}
+            color={isDarkmode ? themeColor.white100 : themeColor.dark}
+          />
+        }
+        leftAction={() => navigation.goBack()}
+        rightContent={
+          <Ionicons
+            name={isDarkmode ? "sunny" : "moon"}
+            size={20}
+            color={isDarkmode ? themeColor.white100 : themeColor.dark}
+          />
+        }
+        rightAction={() => {
+          if (isDarkmode) {
+            setTheme("light");
+          } else {
+            setTheme("dark");
+          }
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* This text using ubuntu font */}
+        <Text fontWeight="bold">Take a picture here</Text>
+
+        <RNCamera
+                    ref={ref => {
+                      this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    androidCameraPermissionOptions={{
+                      title: 'Permission to use camera',
+                      message: 'We need your permission to use your camera',
+                      buttonPositive: 'Ok',
+                      buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                      title: 'Permission to use audio recording',
+                      message: 'We need your permission to use your audio',
+                      buttonPositive: 'Ok',
+                      buttonNegative: 'Cancel',
+                    }}
+                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                      console.log(barcodes);
+                    }}
+                  />
+                  <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+                      <Text style={{ fontSize: 14 }}> SNAP </Text>
+                    </TouchableOpacity>
+                  </View>
+
+
+      </View>
+    </Layout>
+  );
+}
+
+
+
+
+// ---------------------------------------
+// 'use strict';
+// import React, { PureComponent } from 'react';
+// import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import { RNCamera } from 'react-native-camera';
+//
+// class Camera extends PureComponent {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <RNCamera
+//           ref={ref => {
+//             this.camera = ref;
+//           }}
+//           style={styles.preview}
+//           type={RNCamera.Constants.Type.back}
+//           flashMode={RNCamera.Constants.FlashMode.on}
+//           androidCameraPermissionOptions={{
+//             title: 'Permission to use camera',
+//             message: 'We need your permission to use your camera',
+//             buttonPositive: 'Ok',
+//             buttonNegative: 'Cancel',
+//           }}
+//           androidRecordAudioPermissionOptions={{
+//             title: 'Permission to use audio recording',
+//             message: 'We need your permission to use your audio',
+//             buttonPositive: 'Ok',
+//             buttonNegative: 'Cancel',
+//           }}
+//           onGoogleVisionBarcodesDetected={({ barcodes }) => {
+//             console.log(barcodes);
+//           }}
+//         />
+//         <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+//           <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+//             <Text style={{ fontSize: 14 }}> SNAP </Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     );
+//   }
+//
+//   takePicture = async() => {
+//     if (this.camera) {
+//       const options = { quality: 0.5, base64: true };
+//       const data = await this.camera.takePictureAsync(options);
+//       console.log(data.uri);
+//     }
+//   };
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'black',
+//   },
+//   preview: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   capture: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     borderRadius: 5,
+//     padding: 15,
+//     paddingHorizontal: 20,
+//     alignSelf: 'center',
+//     margin: 20,
+//   },
+// });
+//
+// AppRegistry.registerComponent('App', () => Camera);
